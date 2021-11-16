b993892454358e7859f13f39485c0b2fe9aaad75
Jim X. Lin <miljinx@gmail.com>
2021-11-15 23:45:18 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Discord bot using the [discord.js](https://github.com/discordjs/discord.js) library.
+
+Some tweaks were made to get ECMAScript modules and Typescript working properly.
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import "./config";
+import { Client, Intents } from "discord.js";
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.login(process.env.DISCORD_TOKEN);
