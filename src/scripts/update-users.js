71a1a230f199c0c7bf9c5b6ff129be026683297b
Jim X. Lin <miljinx@gmail.com>
2021-12-02 17:29:19 -0800

    Update script to hydrate db

--- src/scripts/update-users.js
+++ src/scripts/update-users.js
@@ -2,21 +2,27 @@ const { DiscordConfig } = require("./config");
 const { Client, Intents } = require("discord.js");
 const { getUserId, createUser, updateUser } = require("../users");
 
+const getInitials = (name) => {
+  if (!name) return;
+  if (!/^[a-z]+\s+[a-z]+$/i.test(name)) return;
+  const [first, last] = name.split(/\s+/);
+  return first[0].toUpperCase() + last[0].toUpperCase();
+};
+
+const populateDatabase = async (members) => {
+  for (const member of members) {
+    const userId = await getUserId(member.id);
+    if (!userId) await createUser(member);
+    await updateUser(member, { initials: getInitials(member.nickname) });
+  }
+};
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.login(DiscordConfig.token);
 
 const guild = client.guilds.cache.get(DiscordConfig.guildId);
 const members = guild.members.list();
 
-(async () => {
-  for (const member of members) {
-    const userId = await getUserId(member.id);
-    if (userId) {
-      await updateUser(member);
-    } else {
-      await createUser(member);
-    }
-  }
-})();
+populateDatabase(members);
 
 client.destroy();
